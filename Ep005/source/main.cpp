c1b2b1581fb763095b70d5d9f5fb32af0f1d8e09
Walter Iddings <walt.iddings@gmail.com>
2021-12-12 14:32:09 -0500

    Fix the directories and files

--- Ep005/source/main.cpp
+++ Ep005/source/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-#include <concepts>  // C++ 20
-
-template <typename T>
-requires std::integral<T>
-T add(T a, T b){
-    return a + b;
-}
+#include "dog.h"
+#include "operations.h"
 
 int main(){
-    std::cout << "Hello from C++ 20 with CMake on Linux/Windows" << std::endl;
-    std::cout << "The sum is: " << add(7, 3) << std::endl;
+
+    Dog dog1("Flitzy");
+    dog1.print_info();
+    
     return 0;
 }
